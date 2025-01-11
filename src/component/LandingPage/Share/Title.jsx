b6825da895aefea5dbb8b83853d724aa0d6ab482
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-01-11 11:54:45 +0600

    careate title for props use

--- src/component/LandingPage/Share/Title.jsx
+++ src/component/LandingPage/Share/Title.jsx
@@ -1,8 +1,18 @@
-const Title = ({ heading, subHeading }) => {
+const Title = ({ subHeading, heading, generalText }) => {
   return (
     <div className="mx-auto text-center md:w-4/12 my-8">
-      <p className="text-yellow-600">{subHeading}</p>
-      <h3 className="text-4xl uppercase border-y-4">{heading}</h3>
+      <div className="bg-[#f5ffe6] inline-flex items-center justify-center px-3 py-1 rounded-lg gap-2">
+        <p className="text-[#749B3F] font-[Rubik]  text-[20px] leading-[23.7px] font-medium tracking-[-1px]">
+          {subHeading}
+        </p>
+      </div>
+      <h3
+        className="text-[48px] leading-[56.88px] font-[Rubik] font-medium tracking-[-2%]"
+        style={{ letterSpacing: "-1px" }}
+      >
+        {heading}
+      </h3>
+      <p className="font-[Questrial] text-center">{generalText}</p>
     </div>
   );
 };
