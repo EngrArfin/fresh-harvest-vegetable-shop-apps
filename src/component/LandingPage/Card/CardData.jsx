676f34c8382e46b0d4e462efaca30054da4ec48f
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-01-11 13:19:21 +0600

    see all product button imletement

--- src/component/LandingPage/Card/CardData.jsx
+++ src/component/LandingPage/Card/CardData.jsx
@@ -2,18 +2,12 @@
 import { useState } from "react";
 import Title from "../Share/Title";
 import CartCard from "./CartCard";
-/* import { Tab, Tabs, TabList } from "react-tabs"; */
 
 const CardData = () => {
-  const [tabMenu, setTabMenu] = useState(0);
-  /*  const [menu] = CategoryData(); */
+  const [showAll, setShowAll] = useState(false);
+  const allCards = Array(12).fill(null);
 
-  /* const all = menu.filter((item) => item.category === "all");
-  const fruts = menu.filter((item) => item.category === "fruts");
-  const vegetable = menu.filter((item) => item.category === "vegetable");
-  const salad = menu.filter((item) => item.category === "salad");
- */
-  const tabTitles = ["All", "Fruts", "Vegetable", "Salad"];
+  const visibleCards = showAll ? allCards : allCards.slice(0, 8);
 
   return (
     <div>
@@ -24,33 +18,33 @@ const CardData = () => {
           "We pride ourselves on offering a wide variety of fresh and flavorful fruits, vegetables, and salad ingredients."
         }
       ></Title>
+
       <div className="navbar-center hidden lg:flex justify-center">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>All</a>
-          </li>
-
-          <li>
-            <a>Fruts</a>
-          </li>
-          <li>
-            <a>Vegetable</a>
-          </li>
-          <li>
-            <a>Salad</a>
-          </li>
+          {["All", "Fruts", "Vegetable", "Salad"].map((title, index) => (
+            <li key={index} onClick={() => console.log(`Switched to ${title}`)}>
+              <a>{title}</a>
+            </li>
+          ))}
         </ul>
       </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 w-[1200px] mx-auto">
-        <CartCard />
-        <CartCard />
-        <CartCard />
-        <CartCard />
-        <CartCard />
-        <CartCard />
-        <CartCard />
-        <CartCard />
+        {visibleCards.map((_, index) => (
+          <CartCard key={index} />
+        ))}
       </div>
+
+      {!showAll && (
+        <div className="flex justify-center mt-6">
+          <button
+            className="btn bg-white text-[#FF6A1A] px-8 py-2 text-lg rounded-md"
+            onClick={() => setShowAll(true)}
+          >
+            See All Products
+          </button>
+        </div>
+      )}
     </div>
   );
 };
